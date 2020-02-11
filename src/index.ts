a955480b7ebbcf305de0d354f7da1ee407e9d765
Ross Perkel <rossperkel@gmail.com>
2020-02-11 16:10:45 -0600

    refactor for type conversion and tuple inclusion on match data

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 // we add the encoding flag to return us a formatted string of the contents of the csv
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
 let manUWins = 0;
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
 
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
